9563985bedd576680525ed25606588961aeb34fb
jeanmichael1993 <jeanmichael760@gmail.com>
2021-03-05 00:18:08 -0300

    Atividade HashSet, encontrando a quantidade de matriculas sem repetir em uma lista de matriculas repetidas

---
+++ HashSetAtividade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetAtividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> turmas = new List<string>() { "A", "B", "C" };
+            HashSet<int> matriculados = new HashSet<int>();
+            foreach (String turma in turmas)
+            {
+               Console.Write($"How many students for course {turma}?");
+               int qtd = int.Parse(Console.ReadLine());
+               for(int i=0;i<qtd;i++)
+                {
+                    matriculados.Add(int.Parse(Console.ReadLine()));
+                }
+                
+            }
+            Console.WriteLine($"Total students:{matriculados.Count}");
+
+        }
+    }
+}
